1e514e7075340947df88c92b18261ba3f08fd5e3
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-04 12:00:31 +0300

    updated: increase score

--- app/src/main/java/com/ismailmesutmujde/kotlincatchthekenny/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlincatchthekenny/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.ismailmesutmujde.kotlincatchthekenny
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.ismailmesutmujde.kotlincatchthekenny.databinding.ActivityMainBinding
 
@@ -8,11 +9,17 @@ import com.ismailmesutmujde.kotlincatchthekenny.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bindingMainActivity: ActivityMainBinding
+    var score = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingMainActivity = ActivityMainBinding.inflate(layoutInflater)
         val view = bindingMainActivity.root
         setContentView(view)
 
+    }
+
+    fun increaseScore(view : View) {
+        score = score + 1
+        bindingMainActivity.scoreText.text = "Score : ${score}"
     }
 }
